fcd9d051099049553d50c3888ad5bd72ca16ad1d
arslanhaider <arslanhaider.dev@hotmail.com>
2024-10-22 14:16:03 +0500

    formatting

--- src/commons/constants.py
+++ src/commons/constants.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class OpenAiModel(Enum):
     GPT_35_TURBO = "gpt-3.5-turbo"
+
     @classmethod
     def has_value(cls, value):
         return value in cls._value2member_map_
